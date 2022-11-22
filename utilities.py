4a9aa75e266a3105a4030e26ead11baaa3f731c0
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-11-22 14:58:08 +0700

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -3,3 +3,5 @@ import streamlit as st
 def load_css():
     with open("style.css") as f:
         st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+    with open("bootstrap.min.css") as g:
+        st.markdown('<style>{}</style>'.format(g.read()), unsafe_allow_html=True)
