a714df8e9df4c016d09f331cb1a627494192ec4d
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-11-23 23:06:49 +0700

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -11,7 +11,9 @@ st.set_page_config(
 #st.header("Welcome to Streamlit Education! 👋")
 #st.write('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet lacus nulla, vitae faucibus erat cursus ut. Nullam quam lorem, semper eu nulla sit amet, pharetra viverra mi. Donec suscipit ligula metus, nec venenatis orci pellentesque et. Quisque ac sem eros. Duis non tellus vel est dictum interdum. Nam pulvinar mattis rhoncus. In sit amet ante ut odio scelerisque ullamcorper. Aliquam hendrerit facilisis purus eu mollis. Maecenas iaculis eget turpis nec mollis.')
 
-st.markdown('''
+image = Image.open('bootstrap-docs.png')
+
+st.markdown(f'''
 <div class="px-4 pt-5 my-5 text-center border-bottom">
     <h1 class="display-4 fw-bold">Centered screenshot</h1>
     <div class="col-lg-6 mx-auto">
@@ -23,11 +25,13 @@ st.markdown('''
     </div>
     <div class="overflow-hidden" style="max-height: 30vh;">
       <div class="container px-5">
-        <img src="https://education.streamlit.app/~/+/media/b289cdc5f56d9f9537eaa2f4ba6040e956abefac179c84800485a1be.png" class="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" width="700" height="500" loading="lazy">
+        {st.image(image)}
       </div>
     </div>
 </div>
 ''', unsafe_allow_html=True)
+
+# <img src="https://education.streamlit.app/~/+/media/b289cdc5f56d9f9537eaa2f4ba6040e956abefac179c84800485a1be.png" class="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" width="700" height="500" loading="lazy">
 
 #image = Image.open('bootstrap-docs.png')
 #st.image(image)
