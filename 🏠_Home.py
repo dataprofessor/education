7e5cc911b6d0da7f4c280a05fd98a333ddb830e9
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-11-23 21:39:25 +0700

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -28,8 +28,8 @@ st.markdown('''
 </div>
 ''', unsafe_allow_html=True)
 
-image = Image.open('bootstrap-docs.png')
-st.image(image)
+#image = Image.open('bootstrap-docs.png')
+#st.image(image)
 
 load_css()
 
