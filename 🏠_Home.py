38e83772a4bd24a9ddafb29463cb915ac3eec585
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-11-24 11:11:59 +0700

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -13,12 +13,13 @@ st.set_page_config(
 
 heroes_title = "Centered screenshot"
 heroes_text = "Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins."
-def heroes():
+
+def heroes(heroes_title_text, heroes_text_text):
     st.markdown(f'''
     <div class="px-4 pt-5 my-5 text-center border-bottom">
-        <h1 class="display-4 fw-bold">{heroes_title}</h1>
+        <h1 class="display-4 fw-bold">{heroes_title_text}</h1>
         <div class="col-lg-6 mx-auto">
-          <p class="lead mb-4">{heroes_text}</p>
+          <p class="lead mb-4">{heroes_text_text}</p>
           <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
             <a href="https://streamlit.io">
                 <button type="button" class="btn btn-primary btn-lg px-4 me-sm-3">Primary button</button>
@@ -29,7 +30,7 @@ def heroes():
     </div>
     ''', unsafe_allow_html=True)
 
-heroes()  
+heroes(heroes_title, heroes_text)  
     
 # <img src="https://education.streamlit.app/~/+/media/c95e53b82610568716fc642535eb5349ca8b2a22f5608e7087a7360c.png" class="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" width="700" height="500" loading="lazy">
 #image = Image.open('streamlit-cover.png')
