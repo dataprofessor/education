37ab33f7d4b7bce6a42f74df9b031e3cdd1a930f
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-12-14 11:33:55 +0700

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -11,7 +11,7 @@ st.set_page_config(
 heroes_title = "Level up your data app <br> building skills"
 heroes_text = """Streamlit Education is a platform providing students and educators access to tools, learning materials and boilerplate code in order to facilitate the learning and teaching of Streamlit. Whether you’re a novice coder or a veteran, there’s something for everyone. 
 <br><br>
-✍️ Register as an Educator or a Student Ambassador:
+✍️ **Register as an Educator or a Student Ambassador:**
 """
 
 def heroes(heroes_title_text, heroes_text_text):
@@ -19,7 +19,7 @@ def heroes(heroes_title_text, heroes_text_text):
     <div class="px-4 pt-5 my-5 text-center border-bottom">
         <h1 class="display-4 fw-bold">{heroes_title_text}</h1>
         <div class="col-lg-6 mx-auto">
-          <p class="lead mb-4"><b>{heroes_text_text}</b></p>
+          <p class="lead mb-4">{heroes_text_text}</p>
           <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
             <a href="https://bit.ly/streamlit-educators">
                 <button type="button" class="btn btn-primary btn-lg px-4 me-sm-3">Educator</button>
