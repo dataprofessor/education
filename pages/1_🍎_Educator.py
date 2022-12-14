33477207e326710467f3aed73242c0b926de5e2b
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-12-14 19:33:39 +0700

    Update 1_🍎_Educator.py

--- pages/1_🍎_Educator.py
+++ pages/1_🍎_Educator.py
@@ -6,6 +6,35 @@ st.set_page_config(page_title="Educator", page_icon="🍎")
 st.header("🍎 Educator")
 
 st.markdown('''
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+''', unsafe_allow_html=True)
+
+st.markdown('''
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet lacus nulla, vitae faucibus erat cursus ut. Nullam quam lorem, semper eu nulla sit amet, pharetra viverra mi. Donec suscipit ligula metus, nec venenatis orci pellentesque et. Quisque ac sem eros. Duis non tellus vel est dictum interdum. Nam pulvinar mattis rhoncus. In sit amet ante ut odio scelerisque ullamcorper. Aliquam hendrerit facilisis purus eu mollis. Maecenas iaculis eget turpis nec mollis.
 
 Fusce ultrices tincidunt elit imperdiet dapibus. Nunc condimentum fermentum lacinia. Duis vestibulum neque posuere lorem tempus iaculis. Nulla ultrices accumsan est quis ullamcorper. Morbi convallis congue dui eu congue. Quisque laoreet turpis commodo luctus varius. Aliquam et iaculis erat. Mauris bibendum sem leo, id ultricies sapien viverra sed. Nunc id erat quis justo sagittis viverra ut id erat.
