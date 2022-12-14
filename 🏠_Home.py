a59fc7a441561665bdbbec58b1e6d6d065a9f3c9
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-12-14 11:06:26 +0700

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -15,11 +15,11 @@ Streamlit Education is a platform providing students and educators access to too
 ✍️ Register as an Educator or a Student Ambassador:
 '''
 
-st.header('Level up your data app building skills')
+st.header('**Level up your data app building skills**')
 
-st.markdown('### Streamlit Education is a platform providing students and educators access to tools, learning materials and boilerplate code in order to facilitate the learning and teaching of Streamlit. Whether you’re a novice coder or a veteran, there’s something for everyone.')
+st.markdown('Streamlit Education is a platform providing students and educators access to tools, learning materials and boilerplate code in order to facilitate the learning and teaching of Streamlit. Whether you’re a novice coder or a veteran, there’s something for everyone.')
 
-st.markdown('## ✍️ Register as an Educator or a Student Ambassador:')
+st.markdown('✍️ Register as an Educator or a Student Ambassador:')
 
 educator = st.button('Educator')
 if educator:
