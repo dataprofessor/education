4073db5b963521c82e85f40c6432332991d80459
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-12-14 19:11:02 +0700

    Update 0_🎁_Benefits.py

--- pages/0_🎁_Benefits.py
+++ pages/0_🎁_Benefits.py
@@ -6,6 +6,7 @@ st.set_page_config(page_title="Benefits", page_icon="🎁")
 st.header("🎁 Benefits")
 
 col1, col2 = st.columns(2)
+col3, col4 = st.columns(2)
 
 with col1:
     st.markdown('''
@@ -17,11 +18,7 @@ with col1:
           <td>
             <ul>
               <li>Access to learning curriculum and presentation slides on getting started with Streamlit.</li>
-            </ul>
-          </td>
-          <td>
-            <ul>
-              <li>App templates to get you up and running in no time.</li>
+              <li>Collaborative contribution to learning materials to help shape them to be the best educational material on Streamlit.</li>
             </ul>
           </td>
         </tr>
@@ -37,11 +34,7 @@ with col2:
             <tr>
               <td>
                 <ul>
-                  <li>Collaborative contribution to learning materials to help shape them to be the best educational material on Streamlit.</li>
-                </ul>
-              </td>
-              <td>
-                <ul>
+                  <li>App templates to get you up and running in no time.</li>
                   <li>Curation of example apps from various domains that can help inspire your own app creations.</li>
                 </ul>
               </td>
@@ -49,52 +42,39 @@ with col2:
         </table>
         ''', unsafe_allow_html=True)
     
+with col3:
+    st.markdown('''
+    <table class="table table-striped">
+        <tr>
+          <th>Tools</td>
+        </tr>
+        <tr>
+          <td>
+            <ul>
+              <li>Additional features/resources for Streamlit Community Cloud.</li>
+              <li>Exclusive pre-release access to new features in Streamlit and Streamlit Community Cloud.</li>
+            </ul>
+          </td>
+        </tr>
+    </table>
+    ''', unsafe_allow_html=True)
     
-    
-st.markdown('''
-<table class="table table-striped">
-    <tr>
-      <th>Learning materials</td>
-      <th>Boilerplate code</td>
-    </tr>
-    <tr>
-      <td>
-        <ul>
-          <li>Access to learning curriculum and presentation slides on getting started with Streamlit.</li>
-          <li>Collaborative contribution to learning materials to help shape them to be the best educational material on Streamlit.</li>
-        </ul>
-      </td>
-      <td>
-        <ul>
-          <li>App templates to get you up and running in no time.</li>
-          <li>Curation of example apps from various domains that can help inspire your own app creations.</li>
-        </ul>
-      </td>
-    </tr>
-</table>
-''', unsafe_allow_html=True)
+with col4:
+    st.markdown('''
+    <table class="table table-striped">
+        <tr>
+          <th>Community</td>
+        </tr>
+        <tr>
+          <td>
+            <ul>
+              <li>Network with fellow student/educator ambassadors.</li>
+              <li>Share best practices.</li>
+          </ul>
+          </td>
+        </tr>
+    </table>
+    ''', unsafe_allow_html=True)  
 
-st.markdown('''
-<table class="table table-striped">
-    <tr>
-      <th>Tools</td>
-      <th>Community</td>
-    </tr>
-    <tr>
-      <td>
-        <ul>
-          <li>Additional features/resources for Streamlit Community Cloud.</li>
-          <li>Exclusive pre-release access to new features in Streamlit and Streamlit Community Cloud.</li>
-        </ul>
-      </td>
-      <td>
-        <ul>
-          <li>Network with fellow student/educator ambassadors.</li>
-          <li>Share best practices.</li>
-        </ul>
-      </td>
-    </tr>
-</table>
-''', unsafe_allow_html=True)
 
 load_css()
