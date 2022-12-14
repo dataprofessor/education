3a2eba7842af6426fcecdd24dff0cbad9759a31f
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-12-14 13:57:23 +0700

    Update 0_🎁_Benefits.py

--- pages/0_🎁_Benefits.py
+++ pages/0_🎁_Benefits.py
@@ -6,12 +6,29 @@ st.set_page_config(page_title="Benefits", page_icon="🎁")
 st.header("🎁 Benefits")
 
 st.markdown('''
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet lacus nulla, vitae faucibus erat cursus ut. Nullam quam lorem, semper eu nulla sit amet, pharetra viverra mi. Donec suscipit ligula metus, nec venenatis orci pellentesque et. Quisque ac sem eros. Duis non tellus vel est dictum interdum. Nam pulvinar mattis rhoncus. In sit amet ante ut odio scelerisque ullamcorper. Aliquam hendrerit facilisis purus eu mollis. Maecenas iaculis eget turpis nec mollis.
-
-Fusce ultrices tincidunt elit imperdiet dapibus. Nunc condimentum fermentum lacinia. Duis vestibulum neque posuere lorem tempus iaculis. Nulla ultrices accumsan est quis ullamcorper. Morbi convallis congue dui eu congue. Quisque laoreet turpis commodo luctus varius. Aliquam et iaculis erat. Mauris bibendum sem leo, id ultricies sapien viverra sed. Nunc id erat quis justo sagittis viverra ut id erat.
-
-Duis ultricies, sem eu fermentum fermentum, nisl tellus egestas odio, at placerat augue quam eget justo. Nullam facilisis porttitor ante, vitae blandit odio dapibus at. Nam tellus arcu, tristique sit amet posuere ut, posuere id nibh. Fusce ut diam dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc metus sapien, feugiat sed diam in, facilisis sollicitudin eros. Quisque quis ex et nunc pellentesque vestibulum. Praesent interdum molestie orci, id faucibus lorem blandit consequat. Vivamus aliquam, erat eu facilisis lacinia, felis nunc tristique lectus, iaculis pretium lectus massa eget risus. Pellentesque rhoncus arcu vel elementum venenatis. Ut in blandit libero, eget facilisis magna. Morbi aliquam elit ut neque imperdiet scelerisque.
-''')
+<table class="table table-striped table-hover">
+    <tr>
+      <th>Learning materials</td>
+      <th>Boilerplate code</td>
+      <th>Tools</td>
+      <th>Community</td>
+    </tr>
+    <tr>
+      <td>
+        Access to learning curriculum and presentation slides on getting started with Streamlit
+      </td>
+      <td>
+        App templates to get you up and running in no time.
+      </td>
+      <td>
+        Additional features/resources for Streamlit Community Cloud.
+      </td>
+      <td>
+        Network with fellow student/educator ambassadors
+      </td>
+    </tr>
+</table>
+''', unsafe_allow_html=True)
 
 st.markdown('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">', unsafe_allow_html=True)
 st.markdown('<i class="bi bi-youtube"></i>', unsafe_allow_html=True)
