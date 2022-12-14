1779493ffa3d2f022539312d35b23db7f90758e6
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-12-14 10:51:08 +0700

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -8,33 +8,19 @@ st.set_page_config(
     layout="wide"
 )
 
-heroes_title = "Level up your data app <br> building skills"
-heroes_text = "Streamlit Education is a platform providing students and educators access to tools, learning materials and boilerplate code in order to facilitate the learning and teaching of Streamlit. Whether you’re a novice coder or a veteran, there’s something for everyone. <br><br>✍️ Register as an Educator or a Student Ambassador:"
+heroes_title = 'Level up your data app <br> building skills'
+heroes_text = '''
+Streamlit Education is a platform providing students and educators access to tools, learning materials and boilerplate code in order to facilitate the learning and teaching of Streamlit. Whether you’re a novice coder or a veteran, there’s something for everyone. 
+<br><br>
+✍️ Register as an Educator or a Student Ambassador:
+'''
 
-def heroes(heroes_title_text, heroes_text_text):
-    st.markdown(f'''
-    <div class="px-4 pt-5 my-5 text-center border-bottom">
-        <h1 class="display-4 fw-bold">{heroes_title_text}</h1>
-        <div class="col-lg-6 mx-auto">
-          <p class="lead mb-4">{heroes_text_text}</p>
-          <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-            <a href="https://bit.ly/streamlit-educators">
-                <button type="button" class="btn btn-primary btn-lg px-4 me-sm-3">Educator</button>
-            </a>
-            <a href="https://bit.ly/streamlit-student-ambassador-app">
-                <button type="button" class="btn btn-outline-secondary btn-lg px-4">Student</button>
-            </a>
-          </div>
-        </div>
-    </div>
-    ''', unsafe_allow_html=True)
+st.markdown(heroes_title, unsafe_allow_html=True)
+st.markdown(heroes_text, unsafe_allow_html=True)
 
-heroes(heroes_title, heroes_text)  
-    
-# <img src="https://education.streamlit.app/~/+/media/c95e53b82610568716fc642535eb5349ca8b2a22f5608e7087a7360c.png" class="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" width="700" height="500" loading="lazy">
-#image = Image.open('streamlit-cover.png')
-#st.write(image)
-
+educator = st.button('Educator')
+if educator:
+    st.markdown('<meta http-equiv="refresh" content="0;url=https://bit.ly/streamlit-educators">', unsafe_allow_html=True)
 
 load_css()
 
